bfbbed0cc532bfe7ac08c339b5bddcee094d8bb6
HeNeugier <github.guidable631@passmail.net>
2026-04-23 22:59:07 +0100

    beginning main.py logic

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+
 def main():
-    print("Hello from web-scraper!")
+    argv_len = len(sys.argv)
+    if argv_len < 2:
+        print("no website provided")
+        sys.exit(1)
+    if argv_len > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    print(f"starting crawl of: {sys.argv[1]}")
 
 
 if __name__ == "__main__":
